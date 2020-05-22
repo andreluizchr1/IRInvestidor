9cb6546ec0be0e3cdff6a7b31ed88540753ecdad
André Rodrigues <andreluizchr1@gmail.com>
2020-05-22 16:54:36 -0300

    Criação da camada de serviço da empresa

---
+++ src/main/java/br/com/investidor/exception/EmpresaDuplicadaException.java
@@ -0,0 +1,36 @@
+package br.com.investidor.exception;
+
+public class EmpresaDuplicadaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmpresaDuplicadaException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpresaDuplicadaException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpresaDuplicadaException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpresaDuplicadaException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmpresaDuplicadaException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
