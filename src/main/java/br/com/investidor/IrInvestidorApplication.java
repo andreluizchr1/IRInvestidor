44e00070857af914540f3c128a191f858ce7c272
André Rodrigues <andreluizchr1@gmail.com>
2020-05-22 16:22:30 -0300

    Projeto Base

---
+++ src/main/java/br/com/investidor/IrInvestidorApplication.java
@@ -0,0 +1,13 @@
+package br.com.investidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrInvestidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrInvestidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/investidor/IrInvestidorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.investidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrInvestidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
